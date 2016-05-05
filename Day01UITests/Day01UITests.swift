77e16ea5e710bbb9bea46da59fbe138a2e420532
blueSky1991 <m18210681011@163.com>
2016-05-05 15:57:12 +0800

    张东东
    修改一些东西

--- Day01UITests/Day01UITests.swift
+++ Day01UITests/Day01UITests.swift
@@ -13,7 +13,7 @@ class Day01UITests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
